97c2422d6bbea8a0436c95d11337a4ec4a5beac4
Leeya Davis <leeya463@gmail.com>
2019-04-07 23:42:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,46 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount, :last_item
+
+  @@items = []
+
+  def initialize (discount = 0)
+    total = 0
+    @total = total
+    @discount = discount
+  end
+
+  def total
+    @total
+    #binding.pry
+  end
+
+  def add_item (item, price, quantity = 1)
+    self.total = @total + (price * quantity)
+    @@items << item
+    @last_item = item
+    #binding.pry
+  end
+
+  def apply_discount
+    if @discount == 0
+      "There is no discount to apply."
+    else
+      with_discount = self.total - (self.total * (@discount.to_f / 100)).round
+      @total = with_discount
+      "After the discount, the total comes to $#{with_discount}."
+    end
+  end
+
+  def items
+    @@items.uniq
+  end
+
+  def void_last_transaction
+    @@items.pop
+    void = self.total - @last_item
+    @total = void
+  end
+
+end
