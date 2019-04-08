263064013fe1486c2853097e346355edac1ff561
Leeya Davis <leeya463@gmail.com>
2019-04-08 00:24:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,6 +17,7 @@ class CashRegister
 
   def add_item (item, price, quantity = 1)
     self.total = @total + (price * quantity)
+    @last_trans = price
     quantity.times do
       @items << item
     end
